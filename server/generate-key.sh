f917c539c74e39847459e68028ba8a504660d69c
VanillaBase1lb <yuvrajmishra7777777@gmail.com>
2021-12-02 08:41:01 +0530

    fixed shebang

--- server/generate-key.sh
+++ server/generate-key.sh
@@ -1,2 +1,2 @@
-#/bin/sh
+#!/bin/sh
 openssl req -x509 -nodes -days 365 -subj "/C=IN/ST=OD/O=dbms_lab/CN=vanillabase1lb" -addext "subjectAltName=DNS:localhost.com" -newkey rsa:2048 -keyout ./selfsigned-key.key -out ./selfsigned-cert.crt;
