50e2aa0dfb39902188ccd1486fc06a441c9e07f8
KickStar35 <73743588+KickStar35@users.noreply.github.com>
2021-12-03 14:33:32 +0530

    Update signup.js

--- client/static/signup.js
+++ client/static/signup.js
@@ -1,14 +1,14 @@
 const URL = 'http://localhost:8080/signup' 
 const data = {
-   "typeEmailX": document.getElementById("typeEmailX"),
-   "typePasswordX": document.getElementById("typePasswordX"),
-   "inputState": document.getElementById("inputState")  
+   "email": document.getElementById("email"),
+   "password": document.getElementById("password"),
+   "user_type": document.getElementById("user_type")  
 };
 // Send a post request
 fetch(URL, {
    method: "POST",
    body: JSON.stringify(data),
    headers: {
-      "Content-type": "application/json; charset=UTF-8"
+      "Content-type": "application/json"
    }
 })
