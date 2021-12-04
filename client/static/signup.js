35e01a1218346e496352593610755b570f708824
Yuvraj Mishra <yuvraj19576@mechyd.ac.in>
2021-12-04 10:07:19 +0530

    Frontend bugfix (#5)
    * partially fixed signup page not being able to send data
    
    * fixed signup bugs and added alert on invalid input

--- client/static/signup.js
+++ client/static/signup.js
@@ -1,14 +1,25 @@
-const URL = 'http://localhost:8080/signup' 
-const data = {
-   "email": document.getElementById("email"),
-   "password": document.getElementById("password"),
-   "user_type": document.getElementById("user_type")  
-};
-// Send a post request
-fetch(URL, {
-   method: "POST",
-   body: JSON.stringify(data),
-   headers: {
-      "Content-type": "application/json"
-   }
-})
+function sendData() {
+    const URL = '/signup' 
+    const data = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        // user_type: document.getElementById("user_type").value,
+        user_type: 0,
+        business_name: "koca",
+        username: "someusername"
+    };
+    // console.log(data)
+    // console.log("test")
+    // Send a post request
+    fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json"
+        }
+    }).then(
+        // window.location.pathname = "/"
+        res => console.log(res)
+        // PENDING read response whether input valid or not
+    )
+}
