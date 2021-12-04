ebdd8d455504af0a870d157d72934f17b6b434a7
Yuvraj Mishra <yuvraj19576@mechyd.ac.in>
2021-12-04 21:11:28 +0530

    refined signup and login, added products api (#6)

--- server/login.js
+++ server/login.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const alert = require("alert")
-let User = require("./database/dbconnector")
+let { User } = require("./database/dbconnector")
 
 async function loginUser(req, res) {
     const user = await User.findOne({username: req.body.username})
     if (user) {
         const password_valid = await bcrypt.compare(req.body.password, user.password_hash)
         if (password_valid) {
-            req.session.userid = req.body.username
-            req.session.usertype = req.body.user_type
+            req.session.userid = user.username
+            req.session.usertype = user.user_type
+            req.session.businessname = user.business_name
             res.send("user logged in")
-            // PENDING redirect to dashboard/home page
+            // PENDING redirect to dashboard or home page
         }
         else {
             // res.send("invalid password")
             alert("invalid password")
+            res.json({msg: "invalid password"})
         }
     }
     else {
         // res.send("invalid username")
         alert("invalid username")
+        res.json({msg: "invalid username"})
     }
 }
 
